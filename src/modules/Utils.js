a0ea0a8dd3aa3d8bd41c581df3a92061e6571d93
kdeary <korbin.deary45@gmail.com>
2021-06-14 00:07:53 -0700

    cleaned makeID for loop

--- src/modules/Utils.js
+++ src/modules/Utils.js
@@ -34,9 +34,7 @@ Utils.makeID = (length=5) => {
 	let text = "";
 	const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-	for (let i = 0; i < length; i++) {
-		text += possible.charAt(Math.floor(Math.random() * possible.length));
-	}
+	for (let i = 0; i < length; i++) text += possible.charAt(Math.floor(Math.random() * possible.length));
 
 	return text;
 }
